4d91b5ef2258665881166cf89be0df590cb0f81c
Kuplatee <kuplatee@gmail.com>
2026-03-01 07:42:11 +0200

    Move Home page content into ui/page component ♻️

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
+import Ui from './ui/page'
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold tracking-widest">
-        TSIRBUNEN POTTERY ADMIN
-      </h1>
-    </main>
-  )
+  return <Ui />
 }
---
+++ src/app/ui/page.tsx
@@ -0,0 +1,9 @@
+export default function Ui() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold tracking-widest">
+        TSIRBUNEN POTTERY ADMIN
+      </h1>
+    </main>
+  )
+}
